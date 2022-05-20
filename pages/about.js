8d5c12668f3d03e28185a18e5b6033ac9b87684e
Pran Pandey <pandey.pran@gmail.com>
2022-05-20 11:07:10 -0600

    Added under construction page for pages currently being. built.

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
+import UnderConstruction from '../public/under_construction.svg'
+import Image from 'next/image'
+import styles from '../styles/About.module.css'
 export default function About(){
 
     return(
-        <div>
-            About Sash Page - COMING SOON! 
+        <div className={styles.container}>
+            <Image src={UnderConstruction} height={500} width={700}/>
+            <p>Page is currently under construction</p>
         </div>
     )
 }
