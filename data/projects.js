3da82969fe8532fa07ad9d8b6bd60eba57635ed7
Pran Pandey <pandey.pran@gmail.com>
2022-05-20 16:46:49 -0600

    Moved all projects into data/projects.js file and cleaned up index page

---
+++ data/projects.js
@@ -0,0 +1,39 @@
+import CoreProjectImage from '../public/images/projects/core.png'
+import HomieProjectImage from '../public/images/projects/homie.png'
+import LisnProjectImage from '../public/images/projects/lisn.png'
+import WorldJournalProjectImage from '../public/images/projects/world-journal.png'
+
+export const allProjects = [
+    {
+        'id': 1,
+        'image': HomieProjectImage,
+        'name': "Rental Housing Search - Homie",
+        'description': "Web application that simplifies and assists the housing rental search process.",
+        'tags': "UX/UI - User Research - Visual Design - Behavioural Design"
+    },
+
+    {
+        'id': 2,
+        'image': LisnProjectImage,
+        'name': "User Research and Onboarding - LISN",
+        'description': "Expanding product market research and improving new user onboarding for a mobile application.",
+        'tags': "UX / UI - User Research - Product Roadmapping - Prototyping - Branding"
+    },
+
+    {
+        'id': 3,
+        'image': CoreProjectImage,
+        'name': "Blood Pressure Monitor - Core.",
+        'description': "Product redesign of a blood pressure monitor with mobile application geared towards athletes.",
+        'tags': "Industrial Design - User Research - UX/UI - Prototyping - 3D CAD"
+    },
+
+    {
+        'id': 4,
+        'image': WorldJournalProjectImage,
+        'name': "NASA Design Sprint - World Journal",
+        'description': "Web application that aims to utilize the satellite imagery and live video feeds from the Internation Space Station for educational purposes.",
+        'tags': "Web Design - Interaction Design - UX/UI"
+    },
+
+]
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,7 @@
 
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import CoreProjectImage from '../public/images/projects/core.png'
-import HomieProjectImage from '../public/images/projects/homie.png'
-import LisnProjectImage from '../public/images/projects/lisn.png'
-import WorldJournalProjectImage from '../public/images/projects/world-journal.png'
-import Link from 'next/link'
-
-const projects = [
-  {
-    'id': 1,
-    'image': HomieProjectImage,
-    'name': "Rental Housing Search - Homie",
-    'description': "Web application that simplifies and assists the housing rental search process.",
-    'tags': "UX/UI - User Research - Visual Design - Behavioural Design"
-  },
-
-  {
-    'id': 2,
-    'image': LisnProjectImage,
-    'name': "User Research and Onboarding - LISN",
-    'description': "Expanding product market research and improving new user onboarding for a mobile application.",
-    'tags': "UX / UI - User Research - Product Roadmapping - Prototyping - Branding"
-  },
-
-  {
-    'id': 3,
-    'image': CoreProjectImage,
-    'name': "Blood Pressure Monitor - Core.",
-    'description': "Product redesign of a blood pressure monitor with mobile application geared towards athletes.",
-    'tags': "Industrial Design - User Research - UX/UI - Prototyping - 3D CAD"
-  },
-
-  {
-    'id': 4,
-    'image': WorldJournalProjectImage,
-    'name': "NASA Design Sprint - World Journal",
-    'description': "Web application that aims to utilize the satellite imagery and live video feeds from the Internation Space Station for educational purposes.",
-    'tags': "Web Design - Interaction Design - UX/UI"
-  },
-
-]
+import { allProjects } from '../data/projects'
 
 
 export default function Home() {
@@ -77,7 +38,7 @@ export default function Home() {
         <section id="my-projects-library">
         <div className={styles.grid}>
 
-          {projects.map((project) => {
+          {allProjects.map((project) => {
             return(
               <div key={project.id} className={styles.card}>
                 <Image src={project.image} alt={project.name} height={400} width={600}></Image>
