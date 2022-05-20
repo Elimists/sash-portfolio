0feab23e7362632b9c18cddff6ccc2d9755b5dae
Pran Pandey <pandey.pran@gmail.com>
2022-05-19 22:15:54 -0600

    Added dynamic data loading functionality.

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,43 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import CoreProjectImage from '../public/images/projects/core.png'
 import HomieProjectImage from '../public/images/projects/homie.png'
-
+import LisnProjectImage from '../public/images/projects/lisn.png'
+import WorldJournalProjectImage from '../public/images/projects/world-journal.png'
 
 
 const projects = [
   {
+    'id': 1,
     'image': HomieProjectImage,
     'name': "Rental Housing Search - Homie",
     'description': "Web application that simplifies and assists the housing rental search process.",
     'tags': ['UX/UI - User Research', 'Visual Design', 'Behavioural Design']
   },
-  
+
+  {
+    'id': 2,
+    'image': LisnProjectImage,
+    'name': "User Research and Onboarding - LISN",
+    'description': "Expanding product market research and improving new user onboarding for a mobile application.",
+    'tags': ['UX / UI', 'User Research', 'Product Roadmapping',  'Prototyping',  'Branding']
+  },
+
+  {
+    'id': 3,
+    'image': CoreProjectImage,
+    'name': "Blood Pressure Monitor - Core.",
+    'description': "Product redesign of a blood pressure monitor with mobile application geared towards athletes.",
+    'tags': "Industrial Design - User Research - UX/UI - Prototyping - 3D CAD"
+  },
+
+  {
+    'id': 4,
+    'image': WorldJournalProjectImage,
+    'name': "NASA Design Sprint - World Journal",
+    'description': "Web application that aims to utilize the satellite imagery and live video feeds from the Internation Space Station for educational purposes.",
+    'tags': "Web Design - Interaction Design - UX/UI"
+  },
+
 ]
 
 
@@ -40,34 +66,18 @@ export default function Home() {
         
 
         <div className={styles.grid}>
-          <div className={styles.card}>
-            <Image src={HomieProjectImage} alt="Homie Project" height={400} width={600}></Image>
-            <h3>Project Title</h3>
-            <p>This is a project description. Temporary description is awesome!</p>
-            <p>UI/UX - User Desgin - Tags</p>
-          </div>
 
-          <div className={styles.card}>
-            <Image src={HomieProjectImage} alt="Homie Project" height={400} width={600}></Image>
-            <h3>Project Title</h3>
-            <p>This is a project description. Temporary description is awesome!</p>
-            <p>UI/UX - User Desgin - Tags</p>
-          </div>
-
-          <div className={styles.card}>
-            <Image src={HomieProjectImage} alt="Homie Project" height={400} width={600}></Image>
-            <h3>Project Title</h3>
-            <p>This is a project description. Temporary description is awesome!</p>
-            <p>UI/UX - User Desgin - Tags</p>
-          </div>
-
-          <div className={styles.card}>
-            <Image src={HomieProjectImage} alt="Homie Project" height={400} width={600}></Image>
-            <h3>Project Title</h3>
-            <p>This is a project description. Temporary description is awesome!</p>
-            <p>UI/UX - User Desgin - Tags</p>
-          </div>
-
+          {projects.map((project) => {
+            return(
+              <div key={project.id} className={styles.card}>
+                <Image src={project.image} alt={project.name} height={400} width={600}></Image>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <p>{project.tags}</p>
+              </div>
+            )
+          })}
+         
         </div>
       </main>
 
