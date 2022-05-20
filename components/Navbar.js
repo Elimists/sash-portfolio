ea5b375c1af71f0b5fc96a8091ad4da0a0710364
Pran Pandey <pandey.pran@gmail.com>
2022-05-20 14:37:03 -0600

    Clicking on logo now takes you to home page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,15 @@ import SashLogo from '../public/sash-logo.svg'
 import Link from 'next/link'
 import Image from 'next/image'
 import {useRouter} from 'next/router'
+
 export default function Navbar(){
     const router = useRouter()
     return(
         <div className={styles.container}>
             <div className={styles.inner_content}>
-
-                <Image src={SashLogo}></Image>
+                <Link href="/">
+                    <a><Image src={SashLogo} /></a>
+                </Link>
                 <div className={styles.nav_link_group}>
                     <Link href='/'><a className={(router.pathname == "/") && styles.nav_links_active}>Home</a></Link>
                     <Link href='/about'><a className={(router.pathname == "/about") && styles.nav_links_active}>About</a></Link>
