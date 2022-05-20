aaeae086e5dedc3afb1aec3b19358dfa06efc3c5
Pran Pandey <pandey.pran@gmail.com>
2022-05-20 10:34:40 -0600

    Added dynamic nav active styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,16 +2,17 @@ import styles from '../styles/Navbar.module.css'
 import SashLogo from '../public/sash-logo.svg'
 import Link from 'next/link'
 import Image from 'next/image'
-import Router from 'next/router'
+import {useRouter} from 'next/router'
 export default function Navbar(){
+    const router = useRouter()
     return(
         <div className={styles.container}>
             <div className={styles.inner_content}>
 
                 <Image src={SashLogo}></Image>
                 <div className={styles.nav_link_group}>
-                    <Link href='/'><a>Home</a></Link>
-                    <Link href='/about'><a>About</a></Link>
+                    <Link href='/'><a className={(router.pathname == "/") && styles.nav_links_active}>Home</a></Link>
+                    <Link href='/about'><a className={(router.pathname == "/about") && styles.nav_links_active}>About</a></Link>
                 </div>
 
             </div>
