488636910272ea86ec7582fc30fdf44198305112
Pran Pandey <pandey.pran@gmail.com>
2022-05-19 21:36:19 -0600

    Adding basic skeleton structure 1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,13 @@
+import styles from '../styles/Navbar.module.css'
+import SashLogo from '../public/sash-logo.svg'
+import Link from 'next/link'
+import Image from 'next/image'
 export default function Navbar(){
     return(
-        <div>
-            Navbar
+        <div className={styles.container}>
+            <Image src={SashLogo}></Image>
+            <Link href='/'><a>Home</a></Link>
+            <Link href='/about'><a>About</a></Link>
         </div>
     )
 }
