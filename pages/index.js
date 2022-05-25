1c8688325895fb60c44d2007a37bb835c58bcce5
Pran Pandey <pandey.pran@gmail.com>
2022-05-25 12:33:24 -0600

    Added tab index to each filter option

--- pages/index.js
+++ pages/index.js
@@ -80,13 +80,13 @@ export default function Home({projects}) {
         
         <section id="my-projects-library" className={styles.my_project_section}>
         <p className={styles.work_heading}>WORK</p>
-        <div className={styles.filter_options}>
-          <div className={(filterTerm == "All") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("All")}>All</div>
-          <div className={(filterTerm == "UI/UX") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("UI/UX")}>UI/UX</div>
-          <div className={(filterTerm == "Industrial Design") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Industrial Design")}>Industrial Design</div>
-          <div className={(filterTerm == "Logos & Branding") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Logos & Branding")}>Logos &#38; Branding </div>
-          <div className={(filterTerm == "Fun") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Fun")}>Fun</div>
-        </div>
+          <div className={styles.filter_options}>
+            <div tabIndex="0" className={(filterTerm == "All") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("All")}>All</div>
+            <div tabIndex="0" className={(filterTerm == "UI/UX") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("UI/UX")}>UI/UX</div>
+            <div tabIndex="0" className={(filterTerm == "Industrial Design") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Industrial Design")}>Industrial Design</div>
+            <div tabIndex="0" className={(filterTerm == "Logos & Branding") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Logos & Branding")}>Logos &#38; Branding </div>
+            <div tabIndex="0" className={(filterTerm == "Fun") ? styles.filter_active : styles.filter} onClick={() => setFilterTerm("Fun")}>Fun</div>
+          </div>
         <div className={styles.grid}>
 
           {projects.map((project) => {
