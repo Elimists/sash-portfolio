df6c87b6f3f19eff08596587da48c0f2d1a283b9
Pran Pandey <pandey.pran@gmail.com>
2022-05-26 13:25:41 -0600

    Testing lottie animation

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,28 @@
 import UnderConstruction from '../public/under_construction.svg'
 import Image from 'next/image'
 import styles from '../styles/About.module.css'
+import animationData from '../public/fun-animation.json'
+
+const defaultOptions = {
+    autoplay: true,
+    animationData: animationData,
+    
+};
 export default function About(){
 
     return(
-        <div className={styles.container}>
-            <Image src={UnderConstruction} height={500} width={700} alt="Under Construction"/>
-            <p>Page is currently under construction</p>
-        </div>
+        <>
+            <div className={styles.lottie_animation}>
+            <lottie-player  src="https://assets8.lottiefiles.com/packages/lf20_pnrpmopy.json" background="transparent" speed="1" autoplay></lottie-player>
+            </div>
+            {/**
+            <div className={styles.container}>
+                <Image src={UnderConstruction} height={500} width={700} alt="Under Construction"/>
+                <p>Page is currently under construction</p>
+            </div>
+         */}
+        </>
+
+       
     )
 }
