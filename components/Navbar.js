d6fc2cb0f9bb664436b98d8b0ee5b047e1ae4d29
Pran Pandey <pandey.pran@gmail.com>
2022-05-26 13:26:30 -0600

    Split the navbar elements into array objects

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,74 @@
 import styles from '../styles/Navbar.module.css'
-import SashLogo from '../public/sash-logo.svg'
 import Link from 'next/link'
 import Image from 'next/image'
 import {useRouter} from 'next/router'
+import {useState, useEffect} from 'react'
 
+import LinkedInLogo from './logos/LinkedinLogo'
+import ThemeToggle from './logos/ThemeToggle'
+import SashLogo from './logos/SashLogo'
+
+
+const navLinks = [
+    {'name': 'Home', 'href': '/'},
+    {'name': 'About', 'href': '/about'},
+
+]
 export default function Navbar(){
     const router = useRouter()
+    const [activeTheme, setActiveTheme] = useState("light")
+
+    function swapTheme(){
+        if (activeTheme == "light"){
+            setActiveTheme("dark")
+        }
+        else{
+            setActiveTheme("light")
+        }
+    }
+    
+    useEffect(() => {
+        document.body.dataset.theme = activeTheme
+    }, [activeTheme])
+
     return(
         <div className={styles.container}>
             <div className={styles.inner_content}>
-                <Link href="/">
-                    <a><Image src={SashLogo} alt="Sash Mahara Logo"/></a>
-                </Link>
+                <div className={styles.sash_logo}>
+                    <Link href="/">   
+                        <a><SashLogo /></a>
+                    </Link>
+                </div>
+
                 <div className={styles.nav_link_group}>
-                    <Link href='/'><a className={(router.pathname == "/") ? styles.nav_links_active : undefined}>Home</a></Link>
-                    <Link href='https://www.linkedin.com/in/sashmahara/?originalSubdomain=ca'><a target="_blank">About</a></Link>
-                    {/*<Link href='/about'><a className={(router.pathname == "/about") ? styles.nav_links_active : undefined}>About</a></Link>*/}
+                    {navLinks.map(links => {
+                        return(
+                        <Link key={links.name} href={links.href}><a className={(router.pathname == links.href) ? styles.nav_links_active : undefined}>{links.name}</a></Link>
+                        )
+                    })}
+                    
+                    <div className={styles.linkedin_logo}>
+                        <Link href="https://www.linkedin.com/in/sashmahara" passHref={true}>
+                            <a target="_blank">
+                                <LinkedInLogo/>
+                            </a>
+                        </Link>
+                    </div>
+
+                    <div className={(activeTheme == "light") ? styles.light_theme : styles.dark_theme} onClick={() => swapTheme()}>
+                        <ThemeToggle/>
+                    </div>
+
+                    <div className={styles.mobile_hamburger_menu}>
+                        <div></div>
+                    </div>
 
                 </div>
 
             </div>
+
+            {/*MOBILE NAV BAR */}
+            
         </div>
     )
 }
