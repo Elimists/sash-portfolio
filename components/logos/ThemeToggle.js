ed0c302867ba3a1af0a63d48cb24ecdc63211b24
Pran Pandey <pandey.pran@gmail.com>
2022-05-26 13:42:09 -0600

    changed toggle svg icon to proper JSX element

--- components/logos/ThemeToggle.js
+++ components/logos/ThemeToggle.js
@@ -2,15 +2,32 @@ import styles from '../../styles/Logo.module.css'
 
 export default function ThemeToggle(){
 
-    return(
-        <svg width="20" height="20" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-            <mask id="path-1-inside-1_339_847" fill="white">
-                <path d="M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z" />
-                <path d="M25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50V0Z" />
+    return (
+        <svg
+            className={styles.theme_toggle_border}
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 50 50"
+        >
+            <mask id="path-1-inside-1_339_847" fill="#fff">
+                <path d="M50 25c0 13.807-11.193 25-25 25S0 38.807 0 25 11.193 0 25 0s25 11.193 25 25z"></path>
+                <path d="M25 0C11.193 0 0 11.193 0 25s11.193 25 25 25V0z"></path>
             </mask>
-            <path d="M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z" fill="white" />
-            <path d="M25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50V0Z" fill="black" />
-            <path d="M25 49C11.7452 49 1 38.2548 1 25H-1C-1 39.3594 10.6406 51 25 51V49ZM1 25C1 11.7452 11.7452 1 25 1V-1C10.6406 -1 -1 10.6406 -1 25H1ZM25 50H23V52H25V50ZM25 0V-2H23V0H25ZM48 25C48 37.7025 37.7025 48 25 48V52C39.9117 52 52 39.9117 52 25H48ZM25 2C37.7025 2 48 12.2975 48 25H52C52 10.0883 39.9117 -2 25 -2V2ZM27 50V0H23V50H27Z" fill={styles.theme_toggle_border} mask="url(#path-1-inside-1_339_847)" />
+            <path
+                fill="#fff"
+                d="M50 25c0 13.807-11.193 25-25 25S0 38.807 0 25 11.193 0 25 0s25 11.193 25 25z"
+            ></path>
+            <path
+                fill="#000"
+                d="M25 0C11.193 0 0 11.193 0 25s11.193 25 25 25V0z"
+            ></path>
+            <path
+                className={styles.theme_toggle_border}
+                fill="#000"
+                d="M25 49C11.745 49 1 38.255 1 25h-2c0 14.36 11.64 26 26 26v-2zM1 25C1 11.745 11.745 1 25 1v-2C10.64-1-1 10.64-1 25h2zm24 25h-2v2h2v-2zm0-50v-2h-2v2h2zm23 25c0 12.703-10.297 23-23 23v4c14.912 0 27-12.088 27-27h-4zM25 2c12.703 0 23 10.297 23 23h4C52 10.088 39.912-2 25-2v4zm2 48V0h-4v50h4z"
+                mask="url(#path-1-inside-1_339_847)"
+            ></path>
         </svg>
-    )
+    );
 }
