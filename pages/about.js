bf35678999c5407f0610cb95fa97968dcb9e91b1
Pran Pandey <pandey.pran@gmail.com>
2022-05-27 15:56:52 -0600

    Changed about page to display 'Under Construction' svg

--- pages/about.js
+++ pages/about.js
@@ -6,15 +6,17 @@ export default function About(){
 
     return(
         <>
+            {/** 
             <div className={styles.lottie_animation}>
             <lottie-player  src="https://assets8.lottiefiles.com/packages/lf20_pnrpmopy.json" background="transparent" speed="1" autoplay></lottie-player>
             </div>
-            {/**
+            */}
+           
             <div className={styles.container}>
                 <Image src={UnderConstruction} height={500} width={700} alt="Under Construction"/>
                 <p>Page is currently under construction</p>
             </div>
-         */}
+       
         </>
 
        
