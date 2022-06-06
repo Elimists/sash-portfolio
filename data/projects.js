a87f252482b66ef99dde71dbae016895e4dd7d93
Pran Pandey <pandey.pran@gmail.com>
2022-06-05 21:53:27 -0600

    Changed max-width for html styling to 1400px and added styling to image enlarge container

--- data/projects.js
+++ data/projects.js
@@ -40,7 +40,7 @@ export const allProjects = [
         tags: "UX/UI - User Research - Visual Design - Behavioural Design",
         duration: "3 Months",
         type: "Personal",
-        role: "Lead Designer",
+        role: "UX Researcher & Designer",
         tools: "Interviews, Surveys, Market Research, Competitive Analysis, Figma",
         overview: "This senior project focsued on researching, designing, and validating a digital service that could help improve the user journey in a chosen problem space. Our group chose to tackle the rental housing experience and develop a solution that could streamline the process for beginners and experienced renters.",
         problem: "Overall, the process of finding and securing a housing rental can be a daunting, overwhelming, and cumbersome journey. Most of the process is learned by word of mouth and through trial and error, making it difficult for inexperienced renters to know where to begin and confidently carry out the entire process. There is also a need for more semi-formal education or resources concerning the renting process as a whole, including knowledge around renter rights. Even more experienced renters encounter a slew of issues relating to poor information across rental ads, usability and experience of rental platforms and listings, communication with landlords and roommates, decision making, and legal matters.",
