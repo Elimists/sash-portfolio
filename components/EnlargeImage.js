c06133fdc43fbe29ca6ed6ff65aa2d8778815a2c
Pran Pandey <pandey.pran@gmail.com>
2022-06-05 22:52:27 -0600

    Added react zoom / pinch package to zoom on images.

--- components/EnlargeImage.js
+++ components/EnlargeImage.js
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import Image from 'next/image'
 import styles from '../styles/EnlargeImage.module.css'
-
+import {TransformWrapper, TransformComponent} from 'react-zoom-pan-pinch'
 
 const backdrop = {
     visible: {
@@ -26,18 +26,30 @@ const closeButton = {
 }
 export default function EnlargeImage({ showModal, setShowModal, imageSource}){
 
-    
+
     return (
         <AnimatePresence exitBeforeEnter>
             {showModal &&
                 <motion.div
+                    id="container"
                     className={styles.container}
                     variants={backdrop}
                     initial="hidden"
-                    animate="visible"
-                    >
+                    animate="visible">
                         {(imageSource != null) &&
-                        <Image src={imageSource} width={imageSource.naturalWidth} height={imageSource.naturalHeight} placeholder="blur" blurDataURL={imageSource}/>
+                        <div id="image-div" className={styles.image_container}>
+                            <TransformWrapper 
+                                defaultScale={1}>
+                                    <TransformComponent>
+                                        <Image 
+                                            src={imageSource} 
+                                            width={imageSource.naturalWidth} 
+                                            height={imageSource.naturalHeight} 
+                                            placeholder="blur"
+                                            blurDataURL={imageSource}/>
+                                    </TransformComponent>
+                            </TransformWrapper>
+                        </div>
                         }
 
                         <motion.div 
