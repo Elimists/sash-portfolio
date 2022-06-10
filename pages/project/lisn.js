8b57baa7c96dd77e718e9c656833b89287996dc6
Pran Pandey <pandey.pran@gmail.com>
2022-06-10 11:55:13 -0600

    Updated each project page to display a 'Under Construction' sign so that users will get some information.

--- pages/project/lisn.js
+++ pages/project/lisn.js
@@ -1,8 +1,13 @@
-
+import uc from '../../public/uc.svg'
+import Image from 'next/image'
+import styles from '../../styles/ProjectDetails.module.css'
+import Link from 'next/link'
 export default function Lisn(){
     return(
-        <div>
-            LISN Page.
+        <div className={styles.container}>
+            <Image src={uc} width={500} height={350} alt="Under Construction" />
+            <p>Items are currentnly being added into the page. Please check back later.</p>
+            <Link href="/"><a>Go Back</a></Link>
         </div>
     )
 }
