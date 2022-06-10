a6c5b1c89d84d8839b3b08628dbc942ca49038d3
Pran Pandey <pandey.pran@gmail.com>
2022-06-09 22:38:16 -0600

    Added data for Lisn project.
    TODO: Finish adding images for Lisn project.

---
+++ data/data.js
@@ -0,0 +1,31 @@
+export const lisnData = {
+    id: 1,
+    title: "Lisn",
+    title_url_safe: "Lisn",
+    short_description: "Improving new user onboarding for a mobile application and UI redesign.",
+    tags: "UX/UI - User Research - Prototyping - Branding ",
+    duration: "2 months",
+    role: "UX/UI Designer",
+    type: "Company Initiative",
+    methods: "User Research, Co-design workshops, Figma",
+    details:[
+        {
+            heading: "Overview",
+            description: "LISN is a startup podcast company developing A.I technologies that help users discover new podcasts and also alow users to create curated playlists using clips from different podcasts. The company is growing its user base and wanted to create an easy to understand onboarding flow for new users. Additionally, they also wanted to design a new home page for their mobile app that facilitated a simplified user-experience. As their new UX/UI designer I lead the research and design of their new onboarding flows and mobile app.",
+        },
+        {
+            heading: "Problem",
+            description: "New users were having a hard time understanding the mobile app as there was no introduction provided on how to use the features and how to create podcast clips which was the main service at the time. The organization of the app also left users confused as their content was scattered in different pages and menus. ",
+        },
+        {
+            heading: "Research",
+            description: "As a new company, LISN did not have previous user experience research or analysis. I began by creating a research plan and working with the team to identify key areas of the service that we needed to understand better. Our main objective was to understand user goals within the app and their satisfaction with the current platform. ",
+            images: [
+                
+            ],
+            subheading: {
+                
+            }
+        }
+    ]
+}
