89ee9e402e35726d81e248d79cd2ed522e89980a
Pran Pandey <pandey.pran@gmail.com>
2022-06-10 20:51:36 -0600

    Core project page 40% completed

--- pages/project/core.js
+++ pages/project/core.js
@@ -9,6 +9,9 @@ import styles from '../../styles/Homie.module.css'
 import CoreTitleSvg from '../../components/svgImages/core/CoreTitleSvg'
 import CoreImage from '../../public/projects/core/core.webp'
 import CoreProcessSvg from '../../components/svgImages/core/CoreProcessSvg'
+import ResearchImage from '../../public/projects/core/research.webp'
+import ProjectionImage from '../../public/projects/core/gbprojections.webp'
+import MarketShareImage from '../../public/projects/core/gbmarketshare.webp'
 
 export default function CoreBP(){
     const [showModal, setShowModal] = useState(false)
@@ -118,6 +121,81 @@ export default function CoreBP(){
                     </div>
                 </section>
                 
+                <section className={styles.research_section}>
+                    <div className={styles.heading_bar}></div>
+                    <h1>Research</h1>
+                    <p>
+                        Through research it was found that many professional athletes actually suffer
+                        from high blood pressure also known as hypertension. In fact hypertension 
+                        was found to be the most common cardiovascular complication affecting 
+                        athletes. Further more sudden cardiac arrest is the leading cause of death 
+                        in young athletes. As a result most athletes today use some type of digital 
+                        tool to track their vitals, training, and recovery to help monitor their 
+                        health and give themselves a competitive edge.
+                    </p>
+                    <div>
+                        <Image
+                            src={ResearchImage}
+                            width={1057}
+                            height={464}
+                            placeholder="blur"
+                            blurDataURL={ResearchImage}
+                            alt="Brain Storm Session"
+                        />
+                    </div>
+                </section>
+
+                <section className={styles.market_econ_section}>
+                    <div className={styles.heading_bar}></div>
+                    <h1>Market Economics</h1>
+                    <p>
+                        Within the research phase, the blood pressure monitor (BPM) market was 
+                        evaluated. BPM market economics data shows that the demand for BPMs is 
+                        projected to increase due to urbanization and modernization worldwide. 
+                        The 1.5 billion dollar industry is projected to grow to a further 2.7 
+                        billion dollars by 2025. Most popular BPM devices on the market today are 
+                        digital monitors.
+                    </p>
+                    <div>
+                        <div>
+                            <Image
+                                src={ProjectionImage}
+                                width={530}
+                                height={281}
+                                placeholder="blur"
+                                blurDataURL={ProjectionImage}
+                                alt="Projection"
+                            />
+                            <p>Global Blood Pressure Monitor Market Size with projections</p>
+                        </div>
+                        <div>
+                            <Image
+                                src={MarketShareImage}
+                                width={530}
+                                height={284}
+                                placeholder="blur"
+                                blurDataURL={MarketShareImage}
+                                alt="Market Share"
+                            />
+                            <p>Global Blood Pressure Monitor Market Share by monitor type</p>
+                        </div>
+                        
+                    </div>
+                </section>
+
+                <section className={styles.analysis_section}>
+                    <div className={styles.heading_bar}></div>
+                    <h1>Analysis</h1>
+                    <p>
+                        Research was done into the life of a professional athlete to see what 
+                        their daily routine was and how they utilize health devices in their 
+                        day to day tasks. After learning more about the specific user, product 
+                        testing was done using a real blood pressure monitor while keeping 
+                        their persona in mind. The goal was to see what the experience would 
+                        be for this person when using a blood pressure monitor.
+                    </p>
+                </section>
+
             </div>
             {(isScrolled) ? <div tabIndex="0" role="button" className={styles.go_top_button} onClick={() => scrollToTheTop()} onKeyDown={(e) => handleOnEnterPressToTop(e)}>&uarr;</div> : null}
         </>
