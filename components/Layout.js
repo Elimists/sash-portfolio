3d54e083403d94655c7f0b9d1c01035e18f85905
Pran Pandey <pandey.pran@gmail.com>
2022-06-12 12:57:33 -0600

    Changed the head title tag to reflect Sash's occupation and added "en" language tag to head

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({children}){
     return(
         <>
             <Head>
-                <title>Sash Mahara Portfolio</title>
+                <title>Sash Mahara | Industrial Designer</title>
                 <meta name="keywords" content="HTML, CSS, JavaScript"/>
                 <meta name="description" content="Sash Mahara is an Industrial designer from Canada specializing in user centered design and research. Sash loves to create amazing experiences through digital or physical products."/>
                 <meta name="author" content="ProReact Labs - Pran Pandey"/>
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="en">
             <Head>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@40,300,0,0" />
