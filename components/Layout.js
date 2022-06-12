6ffa3e78fa8d31ddaeb2c53632b5f61df62e10cb
Pran Pandey <pandey.pran@gmail.com>
2022-06-12 17:09:50 -0600

    updated title tag

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({children}){
     return(
         <>
             <Head>
-                <title>Sash Mahara | Industrial Designer</title>
+                <title>Sash Mahara Industrial Designer</title>
                 <meta name="keywords" content="Industrial Designer, UX UI Designer"/>
                 <meta name="description" content="Sash Mahara is an Industrial designer from Nanaimo, Canada specializing in user centered design and research. Sash loves to create amazing experiences through digital or physical products."/>
                 <meta name="author" content="ProReact Labs - Pran Pandey"/>
