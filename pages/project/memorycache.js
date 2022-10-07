1cc8e9bf6e90bcce4b88b35fcac221b9cec299b8
Pran Pandey <pandey.pran@gmail.com>
2022-10-06 18:51:34 -0600

    corrected ifram properties

--- pages/project/memorycache.js
+++ pages/project/memorycache.js
@@ -326,7 +326,7 @@ export default function CoreBP(){
                         Check out this short video (1m40s) to see how the device works!
                     </p>
                     <div>
-                        <iframe src="https://player.vimeo.com/video/698033014?h=6eb6ceadea" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/698033014?h=6eb6ceadea" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullscreen></iframe>
                         <p><a href="https://vimeo.com/698033014">MemoryCache</a> from <a href="https://vimeo.com/user167769784">Sash Mahara</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
                     </div>
                 </section>
