c8df373a19d39f2165f3bc45940a65916b0da448
Pran Pandey <pandey.pran@gmail.com>
2023-01-23 17:44:29 -0600

    Added escape character for `'`

--- pages/about.js
+++ pages/about.js
@@ -90,7 +90,7 @@ export default function About(){
                         My focus was on user-experience analysis as well as early stage prototyping and concept development.
                     </p>
                     <p>
-                        From 2021 - 2022 I worked as a UX/UI Designer at  LISN, a podcast platform that's developing AI technologies to 
+                        From 2021 - 2022 I worked as a UX/UI Designer at  LISN, a podcast platform that&#39;s developing AI technologies to 
                         enhance podcast discovery and curation. I conducted user research analysis and concept development for 
                         new product features and integration. I lead the design of new customer on-boarding flows and the redesign 
                         of the platform for their LISN 2.0 launch.
