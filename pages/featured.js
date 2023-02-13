760380e93c954fd8cbaa96e50650834447be8dff
Pran Pandey <pandey.pran@gmail.com>
2023-02-12 22:36:43 -0600

    Cleaned up code some more.

--- pages/featured.js
+++ pages/featured.js
@@ -1,4 +1,4 @@
-import FeaturedComponent from "../components/pages/featured/FeaturedComponent"
+import FeaturedComponent from "/components/FeaturedComponent"
 import styles from '../styles/Homie.module.css'
 
 export default function Featured(){
